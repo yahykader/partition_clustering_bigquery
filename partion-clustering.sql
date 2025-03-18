4ec1f966e6ed33d98994deca1b29803fce6fcf8d
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2025-03-18 15:13:28 +0100

    partiton-clustering

---
+++ partion-clustering.sql
@@ -0,0 +1,7 @@
+
+
+select _PARTITIONTIME as pt, _PARTITIONDATE as pd, name from `data-engineering-451818.partition_clustering.name_ingestion_partition`;
+
+select * from `data-engineering-451818.partition_clustering.partition_date`
+
+
